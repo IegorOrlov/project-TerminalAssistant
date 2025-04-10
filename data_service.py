a4f8081f82024cfc85432fea5598d2ccf53a6305
Yegor Orlov <yegor_orlov_tmg@whirlpool.com>
2025-04-11 00:58:13 +0300

    Task6 Task7 Task8

--- data_service.py
+++ data_service.py
@@ -1,13 +1,16 @@
 import pickle
 from address_book import AddressBook
+from note_book import NoteBook
 
-def save_data(book, filename="addressbook.pkl"):
+
+def save_data(*data: tuple, filename="userdata.pkl"):
     with open(filename, "wb") as f:
-        pickle.dump(book, f)
+        pickle.dump(data, f)
 
-def load_data(filename="addressbook.pkl"):
+
+def load_data(filename="userdata.pkl") -> tuple:
     try:
         with open(filename, "rb") as f:
             return pickle.load(f)
     except FileNotFoundError:
-        return AddressBook() 
+        return (AddressBook(), NoteBook())
